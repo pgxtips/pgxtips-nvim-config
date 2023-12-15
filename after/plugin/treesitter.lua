6797453b7021d66c40c024f58995320e4ce4d86c
Brandon <brandongill123@gmail.com>
2023-12-15 00:57:54 +0000

    updated treesitter to ignore vue files

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -2,8 +2,10 @@ require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
   ensure_installed = { "vim", "vimdoc", "query", "javascript", "typescript", "php", "c", "lua", "rust" },
   sync_install = false,
-
   auto_install = true,
+
+  -- List of parsers to ignore installing (or "all")
+  ignore_install = { "vue" },
 
   highlight = {
     enable = true,
