96c6fd85f3558853f03cbefea060962e830aa3a1
pgxtips <brandongill123@gmail.com>
2024-01-12 15:57:40 +0000

    previewer available + smaller paths for telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,15 @@
+local telescope = require('telescope')
+
+telescope.setup({
+    defaults = {
+        path_display = {"truncate"},
+        layout_config = {
+            preview_cutoff = 1,
+        },
+    },
+})
+
 local builtin = require('telescope.builtin')
+
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
