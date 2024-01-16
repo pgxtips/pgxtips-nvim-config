ec81288f87503316109f4a4796ee9b2dbed73dc9
pgxtips <brandongill123@gmail.com>
2024-01-16 17:44:12 +0000

    Added select pasted remap

--- lua/pgxtips/remaps.lua
+++ lua/pgxtips/remaps.lua
@@ -7,4 +7,5 @@ vim.keymap.set("n", "<Tab>", vim.cmd.Explore);
 --centers cursor on screen when page up (c-u) or down (c-d) is pressed 
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
-
+--use gp to select pasted text
+vim.keymap.set("n", "gp", "`[v`]")
